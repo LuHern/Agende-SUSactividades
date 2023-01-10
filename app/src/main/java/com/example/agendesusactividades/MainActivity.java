ea9eae1384c0ee996e709b4ec07cdaa92743eb4b
LuHern <ghjpolrsh@gmail.com>
2023-01-09 15:35:26 -0900

    ellogin

--- app/src/main/java/com/example/agendesusactividades/MainActivity.java
+++ app/src/main/java/com/example/agendesusactividades/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio_sesion_relative);
-        boton = (Button) findViewById(R.id.btn_acceder);
 
+        TextView usuario = (TextView) findViewById(R.id.et_usuario);
+        TextView contrasenia = (TextView) findViewById(R.id.et_password);
+
+        boton = (Button) findViewById(R.id.btn_acceder);
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(usuario.getText().toString().equals("admin") && contrasenia.getText().toString().equals("123")){
+                    Toast.makeText(MainActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, CrearBaseDatos.class));
 
-                startActivity(new Intent(MainActivity.this, CrearBaseDatos.class));
+                } else{
+                    Toast.makeText(MainActivity.this, "Error de autenticacion", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
